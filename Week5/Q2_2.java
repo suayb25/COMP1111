ab7a182a1aef74e95bf53f3f9413af893448af4d
Şuayb Talha Özçelik <43879036+suayb25@users.noreply.github.com>
2021-11-17 12:59:40 +0300

    Add files via upload

---
+++ Week5/Q2_2.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week5;
+
+import java.util.Scanner;
+/**
+ *
+ * @author talha
+ */
+public class Q2_2 {
+    public static void main(String[] args) {
+         Scanner input = new Scanner(System.in);
+        System.out.println("Enter m: ");
+        int m = input.nextInt();
+        System.out.println("Enter n: ");
+        int n = input.nextInt();
+        double upper = 1;
+        double down = 1;
+        for (int i = 1; i <= m; i++) {
+            upper *= i; 
+            if(i<=n){
+                down *= i;
+            }
+            if(i<=m-n){
+               down *= i;  
+            }
+        }
+        System.out.println("Result = " + upper/down);  
+    }
+}
