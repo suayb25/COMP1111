b8e0a501fc110d8d3993b67dcc51abe01b81c934
Şuayb Talha Özçelik <43879036+suayb25@users.noreply.github.com>
2021-12-01 13:22:54 +0300

    Add files via upload

---
+++ Week6/Q10.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week6;
+
+/**
+ *
+ * @author suayb
+ */
+public class Q10 {
+
+    public static void main(String[] args) {
+        int result = factorial(0);
+        result = combination(5, 3);
+        if (result != -1) {
+            System.out.println(result);
+        }else{
+            System.out.println("You sent a negative number! Please try to enter numbers bigger or equal to 0 .");
+        }
+
+    }
+
+    public static int factorial(int N) {
+        if (N < 0) {
+            return -1;
+        } else {
+            int result = 1;
+            for (int i = 2; i <= N; i++) {
+                result = result * i;
+            }
+            return result;
+        }
+    }
+	
+}
