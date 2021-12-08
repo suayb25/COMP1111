58b02a31b4ee63de5a08d8a50329bf89318345be
Şuayb Talha Özçelik <43879036+suayb25@users.noreply.github.com>
2021-12-08 14:04:57 +0300

    Add files via upload

---
+++ Week7/Q10.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week7;
+
+/**
+ *
+ * @author suayb
+ */
+public class Q10 {
+
+    public static void main(String[] args) {
+        displayDigits(123);
+    }
+
+    public static void displayDigits(int N) {
+        if (N == 0) {
+            return;
+        }
+        displayDigits(N / 10);
+        System.out.print(N % 10 + " ");
+    }
+}
