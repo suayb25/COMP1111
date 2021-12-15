004dce61c028d448e4e7337433d6826880779425
Şuayb Talha Özçelik <43879036+suayb25@users.noreply.github.com>
2021-12-15 13:05:49 +0300

    Add files via upload

--- Week8/Q2.java
+++ Week8/Q2.java
@@ -15,19 +15,20 @@ public class Q2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter 6 integers: ");
-        int [] numbers = new int[6];
+        int[] numbers = new int[6];
         for (int i = 0; i < numbers.length; i++) {
             numbers[i] = input.nextInt();
         }
         int max = numbers[0];
         int min = numbers[0];
-          for (int i = 0; i < numbers.length; i++) {
-             if(max < numbers[i])
-                 max = numbers[i];
-             
-             if(min > numbers[i])
-                 min = numbers[i];      
-         }
-          System.out.println("Max: " + max + " , Min:" + min);
+        for (int i = 0; i < numbers.length; i++) {
+            if (max < numbers[i]) {
+                max = numbers[i];
+            }
+            if (min > numbers[i]) {
+                min = numbers[i];
+            }
+        }
+        System.out.println("Max: " + max + " , Min:" + min);
     }
 }
